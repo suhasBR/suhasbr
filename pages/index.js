cdc42443389c817f904ed8867669169367df5d74
suhasBR <suhas.bangalore16@gmail.com>
2023-06-04 19:12:26 +0530

    updated bio

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
         </div>
         <div className='mb-20 flex flex-col items-center'>
           <h2 className='text-4xl text-slate-300 mb-4 font-semibold'>Suhas Sharma BR</h2>
-          <p className='text-xl text-slate-400 mb-8'>Web3 Enthusiast</p>
-          <p className='text-xl text-slate-400 mb-8'>Harmony ZKU Graduate</p>
+          <p className='text-xl text-slate-400 mb-8'>Technology & Business Enthusiast</p>
+          <p className='text-xl text-slate-400 mb-8'>Experienced Solutions Engineer with programming experience</p>
         </div>
       </section>
       <Footer />
